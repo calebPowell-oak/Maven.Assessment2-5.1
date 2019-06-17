86c55d96cabd3293cdc2020f999c2592be1f84e5
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-17 11:55:12 -0400

    Dog and Woofer now pass all tests

--- src/main/java/rocks/zipcode/assessment2/objectorientation/animals/Dog.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/animals/Dog.java
@@ -1,11 +1,13 @@
 package rocks.zipcode.assessment2.objectorientation.animals;
 
+import com.sun.scenario.animation.AnimationPulseMBean;
+
 /**
  * @author leon on 29/11/2018.
  * @ATTENTION_TO_STUDENTS - Ensure that you completed the `Animal` class before attempting this class
  */
-public class Dog {
+public class Dog extends Woofer{
     public String speak() {
-        return null;
+        return "Woof!\nWoof!";
     }
 }
