f33a681302a668813e3c003a66d1e46418d5491c
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-17 11:38:37 -0400

    Person passing all but 1 tests

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -10,35 +10,62 @@ public class Person {
      * @param name - name of person
      * @param address - address of person
      */
+    private Long id;
+    private String name;
+    private Address address;
     public Person(Long id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 
     public Person() {
+        this(Long.MIN_VALUE, "", new Address());
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Address getAddress() {
-        return null;
+        return address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+        if(o instanceof Person){
+            if((((Person)o).getAddress() == null) ||
+                    (((Person)o).getId() == null) ||
+                    (((Person)o).getName() == null))
+                return false;
+            if((address == null) || (id == null) || (name == null))
+                return false;
+            if((this.id.equals(((Person)o).getId()))){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{id="+ id +", name='"+ name +"', address=" + address.toString()+ "}";
     }
 }
