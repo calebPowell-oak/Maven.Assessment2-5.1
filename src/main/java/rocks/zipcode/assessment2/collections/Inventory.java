cfacf0891e73383407bd5f66141768e2b750d906
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-17 09:51:55 -0400

    Finished Collections.Inventory

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,6 +1,8 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Use a map to keep track of inventory in a store
@@ -9,29 +11,39 @@ public class Inventory {
     /**
      * @param strings list of strings to add / remove / fetch from
      */
-    public Inventory(List<String> strings) {
 
+    private Map<String, Integer> inv;
+
+    public Inventory(List<String> strings) {
+         inv = new HashMap<>(0);
+        for (String s : strings){
+            inv.put(s, 0);
+        }
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        inv = new HashMap<>(0);
+        inv.put("", 0);
+        inv.put("Lamp", 0);
+        inv.put("Shade", 0);
+        inv.put("Lightbulb", 0);
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        inv.replace(item, inv.get(item)+1);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        inv.replace(item, inv.get(item)-1);
     }
 
     /**
@@ -39,6 +51,6 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        return inv.get(item);
     }
 }
